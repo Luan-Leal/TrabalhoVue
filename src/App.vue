8ab01cef10c3add5f0cda2ccdca24ff2469074de
Luan Leal <luanleal05@gmail.com>
2022-02-28 22:46:57 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  name: 'Login-page',
+};
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,400;0,600;1,200;1,400;1,600&display=swap');
+* {
+  padding: 0;
+  margin: 0;
+  font-family: 'Poppins', sans-serif;
+  overflow-y: hidden;
+  overflow-x: hidden;
+}
+</style>
