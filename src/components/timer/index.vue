fa7dafa481d0021f384021472d66eeb0ba2ecf17
Luan Leal <luanleal05@gmail.com>
2022-03-01 22:13:23 -0300

    Feat: Timer working

--- src/components/timer/index.vue
+++ src/components/timer/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="timer">
-    <h1 id="timer">600</h1>
+    <h1 id="timer">{{ timerCount }}</h1>
     <h6>segundos</h6>
   </div>
 </template>
@@ -8,6 +8,43 @@
 <script>
 export default {
   name: 'home-timer',
+  data() {
+    return {
+      timerEnabled: true,
+      timerCount: 600,
+    };
+  },
+
+  watch: {
+    timerEnabled(value) {
+      if (value) {
+        setTimeout(() => {
+          this.timerCount--;
+        }, 1000);
+      }
+    },
+
+    timerCount: {
+      handler(value) {
+        if (value > 0 && this.timerEnabled) {
+          setTimeout(() => {
+            this.timerCount--;
+          }, 1000);
+        }
+      },
+      immediate: true,
+    },
+  },
+
+  methods: {
+    play() {
+      this.timerEnabled = true;
+    },
+
+    pause() {
+      this.timerEnabled = false;
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
